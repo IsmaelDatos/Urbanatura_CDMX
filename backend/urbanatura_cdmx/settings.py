39dee386c8073ce19eb5cfde2e06189068f50a9d
IsmaelDatos <ramirez.ismael.datos@gmail.com>
2025-06-17 21:20:09 -0600

    Settings correcto para fly

--- backend/urbanatura_cdmx/settings.py
+++ backend/urbanatura_cdmx/settings.py
@@ -1,134 +1,16 @@
-#----------------------Config-local-mael-----------------------------
-import os
-from pathlib import Path
-
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = 'django-insecure-nj6$+dq6r&tf#^#8#pj6bp5#e03p_t$m72$4d8!z37d$j0!28h'
-DEBUG = True
-ALLOWED_HOSTS = [
-    'localhost',
-    '127.0.0.1',
-    'urbanaturacdmx-production-5786.up.railway.app',
-]
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic',
-    'apps.usuarios',
-    'apps.arbol',
-    'corsheaders', 
-    'apps.poda',
-    'apps.derribo',
-    'apps.trasplante',
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
-]
-CORS_ALLOW_ALL_ORIGINS = True
-ROOT_URLCONF = 'urbanatura_cdmx.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'urbanatura_cdmx/templates')],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'urbanatura_cdmx.wsgi.application'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'urbanatura_db',
-        'USER': 'urbanatura_user',
-        'PASSWORD': 'mael123',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
-USE_I18N = True
-USE_TZ = True
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'urbanatura_cdmx/static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-if not DEBUG:
-    SECURE_HSTS_SECONDS = 31536000 
-    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-    SECURE_HSTS_PRELOAD = True
-    SECURE_SSL_REDIRECT = True
-    SESSION_COOKIE_SECURE = True
-    CSRF_COOKIE_SECURE = True
-    
-AUTH_USER_MODEL = 'usuarios.Usuario'
-
-AUTHENTICATION_BACKENDS = [
-    'django.contrib.auth.backends.ModelBackend',
-]
-
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/'
-
-
-# # ----------------------Config-fly.io-----------------------------
+# #----------------------Config-local-mael-----------------------------
 # import os
 # from pathlib import Path
-# import dj_database_url
-# from dotenv import load_dotenv
 
-# load_dotenv(os.path.join(Path(__file__).resolve().parent.parent.parent, '.env'))
-# BASE_DIR = Path(__file__).resolve().parent.parent.parent
-# SECRET_KEY = os.environ['SECRET_KEY'] 
-# DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+# PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+# BASE_DIR = Path(__file__).resolve().parent.parent
+# SECRET_KEY = 'django-insecure-nj6$+dq6r&tf#^#8#pj6bp5#e03p_t$m72$4d8!z37d$j0!28h'
+# DEBUG = True
 # ALLOWED_HOSTS = [
 #     'localhost',
 #     '127.0.0.1',
-#     '.fly.dev',
-#     'urbanatura-cdmx.fly.dev', 
+#     'urbanaturacdmx-production-5786.up.railway.app',
 # ]
-
 # INSTALLED_APPS = [
 #     'django.contrib.admin',
 #     'django.contrib.auth',
@@ -137,9 +19,9 @@ LOGOUT_REDIRECT_URL = '/'
 #     'django.contrib.messages',
 #     'django.contrib.staticfiles',
 #     'whitenoise.runserver_nostatic',
-#     'corsheaders',
 #     'apps.usuarios',
 #     'apps.arbol',
+#     'corsheaders', 
 #     'apps.poda',
 #     'apps.derribo',
 #     'apps.trasplante',
@@ -148,26 +30,21 @@ LOGOUT_REDIRECT_URL = '/'
 # MIDDLEWARE = [
 #     'django.middleware.security.SecurityMiddleware',
 #     'whitenoise.middleware.WhiteNoiseMiddleware',
-#     'corsheaders.middleware.CorsMiddleware',
 #     'django.contrib.sessions.middleware.SessionMiddleware',
 #     'django.middleware.common.CommonMiddleware',
 #     'django.middleware.csrf.CsrfViewMiddleware',
 #     'django.contrib.auth.middleware.AuthenticationMiddleware',
 #     'django.contrib.messages.middleware.MessageMiddleware',
 #     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+#     'corsheaders.middleware.CorsMiddleware',
 # ]
-# CORS_ALLOW_ALL_ORIGINS = DEBUG 
-# if not DEBUG:
-#     CORS_ALLOWED_ORIGINS = [
-#         "https://urbanatura-cdmx.fly.dev",
-#     ]
-
+# CORS_ALLOW_ALL_ORIGINS = True
 # ROOT_URLCONF = 'urbanatura_cdmx.urls'
 
 # TEMPLATES = [
 #     {
 #         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#         'DIRS': [os.path.join(BASE_DIR, 'backend/urbanatura_cdmx/templates')],
+#         'DIRS': [os.path.join(BASE_DIR, 'urbanatura_cdmx/templates')],
 #         'APP_DIRS': True,
 #         'OPTIONS': {
 #             'context_processors': [
@@ -176,64 +53,187 @@ LOGOUT_REDIRECT_URL = '/'
 #                 'django.contrib.auth.context_processors.auth',
 #                 'django.contrib.messages.context_processors.messages',
 #             ],
-#             'debug': DEBUG,
 #         },
 #     },
 # ]
 
 # WSGI_APPLICATION = 'urbanatura_cdmx.wsgi.application'
+
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.environ.get('DATABASE_URL'),
-#         conn_max_age=600,
-#         conn_health_checks=True,
-#         ssl_require=not DEBUG  
-#     )
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'urbanatura_db',
+#         'USER': 'urbanatura_user',
+#         'PASSWORD': 'mael123',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
 # }
-
 # AUTH_PASSWORD_VALIDATORS = [
-#     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-#     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-#     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-#     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+#     },
 # ]
-
-# LANGUAGE_CODE = 'es-mx'
-# TIME_ZONE = 'America/Mexico_City'
+# LANGUAGE_CODE = 'en-us'
+# TIME_ZONE = 'UTC'
 # USE_I18N = True
 # USE_TZ = True
 # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'backend/urbanatura_cdmx/static')]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'urbanatura_cdmx/static')]
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # MEDIA_URL = '/media/'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # if not DEBUG:
-#     SECURE_HSTS_SECONDS = 2_592_000
+#     SECURE_HSTS_SECONDS = 31536000 
 #     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 #     SECURE_HSTS_PRELOAD = True
 #     SECURE_SSL_REDIRECT = True
-#     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 #     SESSION_COOKIE_SECURE = True
 #     CSRF_COOKIE_SECURE = True
-#     CSRF_TRUSTED_ORIGINS = [
-#         'https://urbanatura-cdmx.fly.dev',
-#         'https://*.fly.dev'
-#     ]
+    
 # AUTH_USER_MODEL = 'usuarios.Usuario'
+
+# AUTHENTICATION_BACKENDS = [
+#     'django.contrib.auth.backends.ModelBackend',
+# ]
+
 # LOGIN_REDIRECT_URL = '/'
 # LOGOUT_REDIRECT_URL = '/'
-# LOGGING = {
-#     'version': 1,
-#     'disable_existing_loggers': False,
-#     'handlers': {
-#         'console': {
-#             'class': 'logging.StreamHandler',
-#         },
-#     },
-#     'root': {
-#         'handlers': ['console'],
-#         'level': 'WARNING',
-#     },
-# }
+
+
+# ----------------------Config-fly.io-----------------------------
+import os
+from pathlib import Path
+import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join(Path(__file__).resolve().parent.parent.parent, '.env'))
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+SECRET_KEY = os.environ['SECRET_KEY'] 
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '.fly.dev',
+    'urbanatura-cdmx.fly.dev', 
+]
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
+    'corsheaders',
+    'apps.usuarios',
+    'apps.arbol',
+    'apps.poda',
+    'apps.derribo',
+    'apps.trasplante',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+CORS_ALLOW_ALL_ORIGINS = DEBUG 
+if not DEBUG:
+    CORS_ALLOWED_ORIGINS = [
+        "https://urbanatura-cdmx.fly.dev",
+    ]
+
+ROOT_URLCONF = 'urbanatura_cdmx.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'backend/urbanatura_cdmx/templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+            'debug': DEBUG,
+        },
+    },
+]
+
+WSGI_APPLICATION = 'urbanatura_cdmx.wsgi.application'
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        conn_health_checks=True,
+        ssl_require=not DEBUG  
+    )
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
+    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+]
+
+LANGUAGE_CODE = 'es-mx'
+TIME_ZONE = 'America/Mexico_City'
+USE_I18N = True
+USE_TZ = True
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'backend/urbanatura_cdmx/static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+if not DEBUG:
+    SECURE_HSTS_SECONDS = 2_592_000
+    SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+    SECURE_HSTS_PRELOAD = True
+    SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    CSRF_TRUSTED_ORIGINS = [
+        'https://urbanatura-cdmx.fly.dev',
+        'https://*.fly.dev'
+    ]
+AUTH_USER_MODEL = 'usuarios.Usuario'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'WARNING',
+    },
+}
