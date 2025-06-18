8aae6e56087c4840eeb0781d5d55d298f7fc99e7
IsmaelDatos <ramirez.ismael.datos@gmail.com>
2025-06-18 00:07:54 -0600

    nada que un indu no haya hecho ya en reddit

--- backend/urbanatura_cdmx/settings.py
+++ backend/urbanatura_cdmx/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'urbanatura_cdmx.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'backend/urbanatura_cdmx/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
